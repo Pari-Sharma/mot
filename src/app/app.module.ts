f939261a23ac7b7ede8e364b4a50528178e08cfa
Pari-Sharma <104079836+Pari-Sharma@users.noreply.github.com>
2022-07-23 14:16:30 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent,
-   
-    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+    AuthModule.forRoot({
+      domain: 'login-mot1.us.auth0.com',
+      clientId: 'ClNJpqQ7tFKfKoY6ki3v0nnKMFgPgR2B'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
